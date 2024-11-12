806997aba749a52a47e9713f8e6e91a1aaa59017
AKSHAI T R <3akshaiachu3@gmail.com>
2024-11-12 09:49:09 +0530

    Create day1 .py
    day 1 
    QN number 169- Majority element

---
+++ day1 .py
@@ -0,0 +1,46 @@
+169. Majority Element
+Solved
+Easy
+Topics
+Companies
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 5 * 104
+-109 <= nums[i] <= 109
+
+
+SOLUTION
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        ans=-1
+        count=0
+
+        for var in nums:
+            if count==0:
+                ans=var
+
+            if ans==var:
+                count=count+1
+            else:
+                count=count-1
+        
+        return ans
